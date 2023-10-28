c9a4b6800f607128cd263b0af9ced6f1b816a72c
naomath <math1972naonao@gmail.com>
2023-10-28 23:25:08 +0900

    Get selected text only in background.js

--- background.js
+++ background.js
@@ -14,15 +14,32 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   const tld = info.menuItemId;
   if(tld!="analyze") return;
   var getSelectText = info.selectionText;
-  let rtnPromise = chrome.tabs.sendMessage(tab.id, "analyze");
-  rtnPromise
-   .then((response)=> {
-        // コールバック関数の処理
-        //nop
-   })
-   .catch((error)=> {
-        // エラー処理
-        //nop
-   });
+  console.log(getSelectText)
 });
+
+// async function callContent(){
+//   let value = await chrome.tabs.sendMessage(tab.id, "analyze",(response)=>{
+//     resolve(response)
+//   });
+//   console.log(value);
+//   rtnPromise
+//    .then((response)=> {
+//         // コールバック関数の処理
+//         //nop
+//         console.log(response)
+//         chrome.windows.create({
+//           url: chrome.runtime.getURL("popup.html"),
+//           type: "popup"
+//         }, function(newWindow) {
+//           chrome.windows.update(newWindow.id, {
+//             width: 590,
+//             height: 645
+//           });
+//         });
+//    })
+//    .catch((error)=> {
+//         // エラー処理
+//         //nop
+//    });
+// }
 
--- content.js
+++ content.js
@@ -2,15 +2,11 @@ console.log("hello");
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     console.log("received");
     if (request == "analyze") {
-        const str = window.getSelection()
-        console.log(str);
-        alert(str);
+        const str = window.getSelection().toString();
+        return new Promise((resolve, reject) => {
+          console.log(str)
+          resolve(str);
+        });
     }
     return true;
   });
-
-  function openDialog() {
-    chrome.windows.create({
-
-    })
-  }
